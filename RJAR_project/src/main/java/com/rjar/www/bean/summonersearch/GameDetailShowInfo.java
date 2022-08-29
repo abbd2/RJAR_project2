24926551029de0238279c999465051551562b7be
lunchBurger <nea22727@gmail.com>
2022-08-29 09:18:21 +0900

    모듈화 진행

--- RJAR_project/src/main/java/com/rjar/www/bean/summonersearch/GameDetailShowInfo.java
+++ RJAR_project/src/main/java/com/rjar/www/bean/summonersearch/GameDetailShowInfo.java
@@ -9,43 +9,38 @@ import lombok.experimental.Accessors;
 public class GameDetailShowInfo {
 
 	// 사용하지 않는 것들은 지울 예정
-	private int profileIconId;
-	private long summonerLevel;
-	private String id;
-	private String name;
-	private String puuid;
+	private String ss_gameId;
+	private int ss_participantId;
+	private int ss_championId;
+	private String ss_championName; // 검색한 소환사의 챔피언 이름
 
 	// ---------------- 게임 내용에 필요한 데이터 ----------------
-	private String ChampionName; // 검색한 소환사의 챔피언 이름
-	private String summonerName; // 소환사 이름
-	private int champId;
-	private int champLevel;
-	private String win; // 경기 결과
+	private int ss_champLevel;
+	private String ss_summonerName; // 소환사 이름
+	private String ss_win; // 경기 결과
 
-	private int summoner1Id; // D 스펠
-	private int summoner2Id; // F 스펠
-	private int ssMain_rune; // 매 인룬
-	private int ssSub_rune;// 서브 룬
+	private int ss_spell1; // D 스펠
+	private int ss_spell2; // F 스펠
+	private int ss_mainRune; // 매 인룬
+	private int ss_subRune;// 서브 룬
 
-	private int kills; // 킬
-	private int deaths; // 데스
-	private int assists; // 어시스트
-	private double kda;
-	private double killParticipation; // 해당 게임의 킬 관여율
-	private int totalDamageDealtToChampions; // 챔피언 딜량
-	private int totalDamageTaken; // 챔피언 받은 피해량
+	private int ss_kills; // 킬
+	private int ss_deaths; // 데스
+	private int ss_assists; // 어시스트
+	private double ss_killParticipation; // 해당 게임의 킬 관여율
+	private int ss_totalDamageDealtToChampions; // 챔피언 딜량
+	private int ss_totalDamageTaken; // 챔피언 받은 피해량
 
-	private int visionWardsBoughtIngame;
-	private int wardsKilled;
-	private int wardsPlaced;
-	private int totalMinionsKilled; // cs
+	private int ss_visionWardBuy;
+	private int ss_wardKilled;
+	private int ss_wardPlaced;
+	private int ss_cs; // cs
 
-	private int item0; // 0번 아이템 슬롯
-	private int item1; // 1번 아이템 슬롯
-	private int item2; // 2번 아이템 슬롯
-	private int item3; // 3번 아이템 슬롯
-	private int item4; // 4번 아이템 슬롯
-	private int item5; // 5번 아이템 슬롯
-	private int item6; // 6번 아이템 슬롯
+	private String ss_items; // 0번 아이템 슬롯
+	private int ss_earnGold;
+	private int ss_dragon;
+	private int ss_baron;
+	private int ss_tower;
+
 
 }
---
+++ RJAR_project/src/main/java/com/rjar/www/dao/ISummonerSearch.java
@@ -0,0 +1,9 @@
+package com.rjar.www.dao;
+
+import com.rjar.www.bean.summonersearch.GameDetailShowInfo;
+
+public interface ISummonerSearch {
+
+	GameDetailShowInfo SearchGame(String ss_gameId);
+
+}
